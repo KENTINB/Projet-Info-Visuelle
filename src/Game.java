a0e0004a4695f8b61d4370670c025a828ed8e649
KENTINB <krentin@hotmail.fr>
2016-03-08 17:25:50 +0100

    blalbla assignement3

--- src/Game.java
+++ src/Game.java
@@ -1,8 +1,10 @@
 import processing.core.*;
+import processing.event.MouseEvent;
 
 public class Game extends PApplet {
 	float angleX = 0;
 	float angleZ = 0;
+	float con = 1;
 
 	public void settings() {
 		size(500, 500, P3D);
@@ -15,29 +17,42 @@ public class Game extends PApplet {
 	public void draw() {
 		background(200);
 		lights();
-		camera(0, 0, 450, 250, 250, 0, 0, 1, 0);
+		camera(500, 500, 0, width/2, height/2, 0, 0, 1, 0);
 		translate(width / 2, height / 2, 0);
 		rotateZ(angleX);
 		rotateX(angleZ);
 		box(200, 20, 200);
 	}
 
+	public float clamp(float x,float min, float max){
+		if(x>max){return max;}
+		else if(x<min){return min;}
+		return x;
+	}
+
 	public void mouseDragged() {
-		float dX = (mouseX - pmouseX);
-		float dZ = (mouseY - pmouseY);
+
+		float dZ = (mouseX - pmouseX);
+		float dX = (mouseY - pmouseY);
 		if (dX > 1.0) {
-			angleX += PI / 10;
+			angleX= clamp(angleX+con*PI/24,-PI/3,PI/3);
 		} else if (dX < -1.0) {
-			angleX -= PI / 10;
+			angleX= clamp(angleX-con*PI/24,-PI/3,PI/3);
 		} else if (dZ > 1.0) {
-			angleZ += PI / 10;
+			angleZ= clamp(angleZ+con*PI/24,-PI/3,PI/3);
 		} else if (dZ < -1.0) {
-			angleZ -= PI / 10;
+			angleZ= clamp(angleZ-con*PI/24,-PI/3,PI/3);
+
 		}
+	}
+
+	public void mouseWheel(MouseEvent event) {
+		float e = event.getCount();
+		if(e > 0) con *= 1.05;
+		else con *= 0.95;
 	}
 
 	public static void main(String[] args) {
 		PApplet.main(new String[] { "Game" });
 	}
-
 }
